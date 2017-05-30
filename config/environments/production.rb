feba5ebf243a5216f68943e008360c72bbecb13e
nickanastasiades <Nick@dhcp-10-105-159-217.wireless.northwestern.private>
2017-05-30 11:26:54 -0500

    commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "smtp.google.com",####important
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "llobetest@gmail.com",
+    password: "LLOBETEST!"
+  }
 end
