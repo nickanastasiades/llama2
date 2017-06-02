d03b7559c9b2def37946fdc16e76f2a2095f8eb0
nickanastasiades <Nick@dhcp-10-105-143-154.wireless.northwestern.private>
2017-06-02 10:46:10 -0500

    j

--- app/controllers/clients/confirmations_controller.rb
+++ app/controllers/clients/confirmations_controller.rb
@@ -25,5 +25,4 @@ class Clients::ConfirmationsController < Devise::ConfirmationsController
   # def after_confirmation_path_for(resource_name, resource)
   #   super(resource_name, resource)
   # end
-
 end
