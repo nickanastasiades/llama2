5c173a7106373cedd77ab74097c77d7278777b4a
nickanastasiades <Nick@Nicks-MacBook.local>
2017-06-09 14:42:32 -0700

    successful active admin

--- app/controllers/clients/registrations_controller.rb
+++ app/controllers/clients/registrations_controller.rb
@@ -50,13 +50,10 @@ class Clients::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
 
   #The path used after sign up for inactive accounts.
+
   def after_inactive_sign_up_path_for(resource)
   "/confirm_email"
-
- end
+  end
 end
